e972c2790fa545137b9687fa07d385fa7da81b8c
Daniel Felix <felixdaniel2056@gmail.com>
2022-11-10 09:43:27 +0100

    chore: third major update 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,60 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('db_dump', function () {
+    /*
+    Needed in SQL File:
+
+    SET GLOBAL sql_mode = '';
+    SET SESSION sql_mode = '';
+    */
+    $get_all_table_query = "SHOW TABLES";
+    $result = DB::select(DB::raw($get_all_table_query));
+
+    $tables = [
+        'products'
+    ];
+
+    $structure = '';
+    $data = '';
+    foreach ($tables as $table) {
+        $show_table_query = "SHOW CREATE TABLE " . $table . "";
+
+        $show_table_result = DB::select(DB::raw($show_table_query));
+
+        foreach ($show_table_result as $show_table_row) {
+            $show_table_row = (array)$show_table_row;
+            $structure .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
+        }
+        $select_query = "SELECT * FROM " . $table;
+        $records = DB::select(DB::raw($select_query));
+
+        foreach ($records as $record) {
+            $record = (array)$record;
+            $table_column_array = array_keys($record);
+            foreach ($table_column_array as $key => $name) {
+                $table_column_array[$key] = '`' . $table_column_array[$key] . '`';
+            }
+
+            $table_value_array = array_values($record);
+            $data .= "\nINSERT INTO $table (";
+
+            $data .= "" . implode(", ", $table_column_array) . ") VALUES \n";
+
+            foreach($table_value_array as $key => $record_column)
+                $table_value_array[$key] = addslashes($record_column);
+
+            $data .= "('" . implode("','", $table_value_array) . "');\n";
+        }
+    }
+    $file_name = __DIR__ . '/../database/' . $table . '-' . date('y_m_d') . '.sql';
+    $file_handle = fopen($file_name, 'w + ');
+
+    $output = $structure . $data;
+    fwrite($file_handle, $output);
+    fclose($file_handle);
+    echo "DB backup ready";
+});
 
 Route::get('/', 'HomeController@index')->name('index');
 Route::get('about-us', 'HomeController@about')->name('about');
